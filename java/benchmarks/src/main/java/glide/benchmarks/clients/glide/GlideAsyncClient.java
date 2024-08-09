5b39e863d31ccaae6768eb04769974beea98a2b7
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-08-08 17:47:52 -0700

    Experiment
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/benchmarks/src/main/java/glide/benchmarks/clients/glide/GlideAsyncClient.java
+++ java/benchmarks/src/main/java/glide/benchmarks/clients/glide/GlideAsyncClient.java
@@ -46,6 +46,7 @@ public class GlideAsyncClient implements AsyncClient<String> {
                                             .host(connectionSettings.host)
                                             .port(connectionSettings.port)
                                             .build())
+                            .requestTimeout(5000)
                             .useTLS(connectionSettings.useSsl)
                             .build();
 
